c0e7f696c3a31389b949a7ccbeb30d440b2b67fb
Alexey Mileev <alemileev@gmail.com>
2023-10-03 18:46:01 +0400

    Font size update

--- config.el
+++ config.el
@@ -37,9 +37,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Font
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14)
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 12)
       doom-variable-pitch-font
-        (font-spec :family "JetBrainsMono Nerd Font" :size 14)
+        (font-spec :family "JetBrainsMono Nerd Font" :size 12)
       doom-big-font (font-spec :family "JetBrainsMono Nerd Font" :size 23))
 (after! doom-themes
   (setq doom-themes-enable-bold t
